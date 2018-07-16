87e900734008e03d5ea37ef3e2a9d5462bd6f166
zbianca <bianca.zanardi@gmail.com>
2018-07-16 19:24:03 +0200

    Insert map with markers of the stations

---
+++ src/Map.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { withGoogleMap, GoogleMap, Marker, InfoWindow } from 'react-google-maps';
+
+class Map extends Component {
+
+  render() {
+
+    const GoogleMapExample = withGoogleMap(props => (
+      <GoogleMap
+        defaultCenter = { { lat: 35.69406568779301, lng: 139.7380327592773 } }
+        defaultZoom = { 12 }
+      >
+
+        {this.props.stations.map(station => <Marker key={station.id} name={station.name} position={station.position} />)}
+
+      </GoogleMap>
+    ));
+
+    return(
+      <div>
+        <GoogleMapExample
+          containerElement={ <div style={{ height: '80vh', width: '100vw' }} /> }
+          mapElement={ <div style={{ height: '100%' }} /> }
+        />
+      </div>
+    );
+
+  }
+};
+
+export default Map;
