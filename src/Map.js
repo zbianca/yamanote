1dab62be93c53a6fcda7f3d0c809cd2a4f9642a9
zbianca <bianca.zanardi@gmail.com>
2018-07-23 19:22:04 +0200

    render one infowindow

--- src/Map.js
+++ src/Map.js
@@ -7,7 +7,19 @@ import {
 } from 'react-google-maps';
 
 class Map extends Component {
+
+  state = {
+    infoOpen: false,
+    infoWindow: '',
+  }
+
+  showInfo = station => {
+    this.setState({infoOpen: true, infoWindow: station});
+  };
+
   render() {
+    const infoOpen = this.state.infoOpen;
+    const infoWindow = this.state.infoWindow;
     const GoogleMapExample = withGoogleMap(props => (
       <GoogleMap
         defaultCenter={{ lat: 35.69406568779301, lng: 139.7380327592773 }}
@@ -19,7 +31,14 @@ class Map extends Component {
             name={station.name}
             position={station.position}
             visible={station.visible}
-          />
+            onClick={()=> this.showInfo(station.id)}
+          >
+            {station.visible && infoOpen && infoWindow === station.id && (
+              <InfoWindow>
+                <div>{station.name}</div>
+              </InfoWindow>
+            )};
+          </Marker>
         ))}
       </GoogleMap>
     ));
