8f80ec198ee1a9a491643409e6d57b99343357a1
zbianca <bianca.zanardi@gmail.com>
2018-07-26 00:38:22 +0200

    fetch wikipedia data

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ class App extends Component {
   state = {
     stations,
     infoWindow: '',
+    content: {},
   };
 
   filterMarkers = filteredStations => {
@@ -20,8 +21,24 @@ class App extends Component {
     this.setState({ stations: updatedStations });
   };
 
-  showInfo = station => {
-    this.setState({ infoWindow: station });
+  showInfo = (id, wiki) => {
+    this.setState({ infoWindow: id });
+    if (!this.state.content.hasOwnProperty(wiki)) {
+      this.getWiki(wiki);
+    }
+  };
+
+  getWiki = station => {
+    fetch(`https://en.wikipedia.org/api/rest_v1/page/summary/${station}`)
+    .then(r => r.json())
+    .then(r => this.saveWiki(station, r.thumbnail.source, r.extract))
+    .catch(e => console.error(e));
+  };
+
+  saveWiki = (station, thumb, paragraph) => {
+    const content = { ...this.state.content };
+    content[station] = { thumb, paragraph };
+    this.setState({ content });
   };
 
   render() {
@@ -37,6 +54,7 @@ class App extends Component {
           stations={this.state.stations}
           showInfo={this.showInfo}
           infoWindow={this.state.infoWindow}
+          content={this.state.content}
         />
       </div>
     );
--- src/Map.js
+++ src/Map.js
@@ -17,15 +17,15 @@ const MyGoogleMap = withGoogleMap(props => (
         name={station.name}
         position={station.position}
         visible={station.visible}
-        onClick={() => props.showInfo(station.id)}
+        onClick={() => props.showInfo(station.id,station.wiki)}
       >
         {station.visible &&
           props.infoWindow === station.id && (
             <InfoWindow key={station.id}>
               <div className="Info">
-              <h2>{station.name}</h2>
-              <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4b/Tamachistation-mitaentrance-westentrance-jan27-2015.jpg/320px-Tamachistation-mitaentrance-westentrance-jan27-2015.jpg"alt="{station.name}" />
-              <p><b>Tokyo Station</b> is a railway station in the Chiyoda City, Tokyo, Japan. The original station is located in Chiyoda's Marunouchi business district near the Imperial Palace grounds. The newer Eastern extension is not far from the Ginza commercial district. Due to its large area covered, the station is divided into Marunouchi and Yaesu sides in its directional signage.</p>
+                <h2 className="Info-title">{station.name}</h2>
+                <img src="samehere" alt="{station.name}" />
+                <p>somehow get props.content[station.wiki].paragraph after mount</p>
               </div>
             </InfoWindow>
           )}
@@ -35,6 +35,7 @@ const MyGoogleMap = withGoogleMap(props => (
 ));
 
 class Map extends Component {
+
   render() {
     const mapWidth = window.innerWidth - 215
     return (
@@ -44,6 +45,7 @@ class Map extends Component {
         showInfo={this.props.showInfo}
         stations={this.props.stations}
         infoWindow={this.props.infoWindow}
+        content={this.props.content}
         />
     );
   }
